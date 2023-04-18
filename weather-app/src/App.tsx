d443bcbbcd9732f9421059f03dddf85569e9562a
Jarek Bigaj <j.bigaj@interia.pl>
2023-04-18 18:58:34 +0200

    Add first Components and add some styles

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { HtmlHTMLAttributes } from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  children?: React.ReactNode;
+  className?: string;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WeatherAppWrapper>
+      <CityWrapper>
+        <CityNameField/>
+        <CitySearchField/>
+      </CityWrapper>
+      <WeahterInfoWrapper>
+        <DateTime/>
+        <WeatherCode/>
+        <Temperature/>
+        <Wind/>
+      </WeahterInfoWrapper>
+    </WeatherAppWrapper>
   );
 }
+
+const WeatherAppWrapper= styled(({className,children}:Props)=>{
+  return <div className={className}>{children}</div>
+})`
+  position: relative;
+  text-align:left;
+  margin: 2rem auto;
+  width: 60rem;
+  height: auto;
+  border-radius: 5px;
+  background-color: (206, 52%, 80%, 1);
+  box-shadow: 3px 5px 3px 5px;
+`;
+
+const CityWrapper = styled(({children, className}:Props)=>{
+  return <div className={className}>{children}</div>
+})`
+  position: relative;
+  text-align:left;
+  margin: none;
+  width: auto;
+  height: auto;
+  padding: 10px;
+  border-radius: 5px 5px 0px 0px;
+  background-color: hsla(202, 100%, 29%, 1);
+  box-shadow: 1px 1px 1px 1px;
+`;
+
+const CityNameField = styled(({className}:Props) =>{
+  return <h1 className={className}>Warszawa</h1>
+})`
+`;
+
+const CitySearchField = styled(({className}:Props)=>{
+  return <input className={className} type="text"/>
+})`
+`;
+
+const WeahterInfoWrapper = styled(({className,children}:Props) => {
+  return <div className={className}>{children}</div>
+})`
+`;
+
+const DateTime = styled(({className}:Props) => {
+  return <span className={className}>Data i czas</span>
+})`
+`;
+
+const WeatherCode = styled(({className}:Props) => {
+  return <span className={className}>weatherCode</span>
+})`
+`;
+const Temperature = styled(({className}:Props) => {
+  return <span className={className}>temp</span>
+})`
+`;
+const Wind = styled(({className}:Props) => {
+  return <div className={className}>Wind</div>
+})`
+`;
 
 export default App;
